62d3b1d13696a0288dd364c9b90ae3b6192e06c0
Tom Osborne <tom.j.osborne@gmail.com>
2022-09-03 23:59:09 +0100

    refactor(server): Small refactor

--- server/main.lua
+++ server/main.lua
@@ -1,19 +1,15 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent('qb-speedcamera:PayBill')
-AddEventHandler('qb-speedcamera:PayBill', function(amount)
-	print("Amount:", amount)
+RegisterServerEvent('qb-speedcamera:PayBill', function(amount)
 	local xPlayer = QBCore.Functions.GetPlayer(source)
 	xPlayer.Functions.RemoveMoney("bank", amount)
 end)
 
 
-RegisterServerEvent('qb-speedcamera:openGUI')
-AddEventHandler('qb-speedcamera:openGUI', function()
+RegisterServerEvent('qb-speedcamera:openGUI', function()
 	TriggerClientEvent('qb-speedcamera:openGUI', source)
 end)
 
-RegisterServerEvent('qb-speedcamera:closeGUI')
-AddEventHandler('qb-speedcamera:closeGUI', function()
+RegisterServerEvent('qb-speedcamera:closeGUI', function()
 	TriggerClientEvent('qb-speedcamera:closeGUI', source)
 end)
